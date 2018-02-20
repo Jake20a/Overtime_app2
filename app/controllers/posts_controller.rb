6b6ee9bbb82c1124f8ad51649f13ee5194fe5ed0
jake20a <jakedevcamp@gmail.com>
2018-02-20 11:48:48 -0700

    show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,23 @@
 class PostsController < ApplicationController
-  def index
-  end
+	before_action :set_post, only: [:show]
+	def index
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(params.require(:post).permit(:date, :rationale))
+
+		@post.save
+
+		redirect_to @post
+		
+	end
+
+	def show
+	@post = Post.find(params[:id])
+
+	end
 end
