34991e51d689ba53a6f53d32ab194100aee2bfe5
jake20a <jakedevcamp@gmail.com>
2018-02-20 12:01:20 -0700

    creation processs

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -19,16 +19,12 @@ describe 'navigate' do
     end
 
     it 'has a new form that can be reached' do
-      visit new_post_path
       expect(page.status_code).to eq(200)
     end
 
     it 'can be created from new form page' do
-        visit new_post_path
-
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
-      
       click_on "Save"
 
       expect(page).to have_content("Some rationale")
