ed40a92114e66a97b6b381ef40861ffb78df2fd0
jake20a <jakedevcamp@gmail.com>
2018-02-26 15:05:58 -0700

    admin dashboard

--- app/dashboards/admin_user_dashboard.rb
+++ app/dashboards/admin_user_dashboard.rb
@@ -4,20 +4,20 @@ require "administrate/base_dashboard"
 
 class AdminUserDashboard < Administrate::BaseDashboard
   ATTRIBUTE_TYPES = {
-    posts: Field::HasMany,.with_options(searchable: false)
-    id: Field::Number,.with_options(searchable: false)
-    email: Field::String,.with_options(searchable: true)
-    password: Field::String,.with_options(searchable: false)
-    sign_in_count: Field::Number,.with_options(searchable: false)
-    current_sign_in_at: Field::DateTime,.with_options(searchable: false)
-    last_sign_in_at: Field::DateTime,.with_options(searchable: false)
+    posts: Field::HasMany.with_options(searchable: false),
+    id: Field::Number.with_options(searchable: false),
+    email: Field::String.with_options(searchable: true),
+    password: Field::String.with_options(searchable: false),
+    sign_in_count: Field::Number.with_options(searchable: false),
+    current_sign_in_at: Field::DateTime.with_options(searchable: false),
+    last_sign_in_at: Field::DateTime.with_options(searchable: false),
     current_sign_in_ip: Field::String.with_options(searchable: false),
     last_sign_in_ip: Field::String.with_options(searchable: false),
-    first_name: Field::String,.with_options(searchable: false)
-    last_name: Field::String,.with_options(searchable: false)
-    created_at: Field::DateTime,.with_options(searchable: false)
-    updated_at: Field::DateTime,.with_options(searchable: false)
-    type: Field::String,.with_options(searchable: false)
+    first_name: Field::String.with_options(searchable: false),
+    last_name: Field::String.with_options(searchable: false),
+    created_at: Field::DateTime.with_options(searchable: false),
+    updated_at: Field::DateTime.with_options(searchable: false),
+    type: Field::String.with_options(searchable: false),
   }.freeze
 
   COLLECTION_ATTRIBUTES = [
