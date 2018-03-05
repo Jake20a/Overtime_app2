67b82f64d4c1a269c66775c53d3f6f314442dc38
jake20a <jakedevcamp@gmail.com>
2018-03-05 14:58:45 -0700

    update phone status

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,6 +9,7 @@ FactoryBot.define do
     email { generate :email }
     password "asdfasdf"
     password_confirmation "asdfasdf"
+    phone "5555555555"
   end
 
   factory :admin_user, class: "AdminUser" do
@@ -17,13 +18,15 @@ FactoryBot.define do
     email { generate :email }
     password "asdfasdf"
     password_confirmation "asdfasdf"
+    phone "5555555555"
   end
-  
+
   factory :non_authorized_user, class: "User" do
-  first_name 'Non'
-  last_name 'Authorized'
-  email { generate :email }
-  password "asdfasdf"
-  password_confirmation "asdfasdf"
+    first_name 'Non'
+    last_name 'Authorized'
+    email { generate :email }
+    password "asdfasdf"
+    password_confirmation "asdfasdf"
+    phone "5555555555"
   end
 end
